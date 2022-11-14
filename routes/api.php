6861d7436f53986f62a6e6f4b874b903068a6bf3
Alansan <alaansannchezz@gmail.com>
2022-11-14 17:58:13 -0300

    feat: Se creo la api para login logout

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/user/login', 'login');
+});
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/user/profile', [AuthController::class, 'userProfile']);    
+    Route::get('/logout', [AuthController::class, 'logout']);    
 });
